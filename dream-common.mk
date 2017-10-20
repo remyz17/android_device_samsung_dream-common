e18d5700335d692ced9169b53aa457eec25e1194
remyz17 <remy.zulauff@gmail.com>
2017-10-20 12:21:54 +0200

    dream : call SLSI and proprietary packages

--- dream-common.mk
+++ dream-common.mk
@@ -59,3 +59,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# call Samsung SLSI board support package
+$(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
+$(call inherit-product, hardware/samsung_slsi-cm/exynos8895/exynos8895.mk)
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/dream-common/dream-common-vendor.mk)
