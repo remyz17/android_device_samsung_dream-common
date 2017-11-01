ef18c8d2e146ccf366e07fbc42f21d48cddabc50
remyz17 <remy.zulauff@gmail.com>
2017-11-01 12:52:54 +0100

    dream: include libbauthtzcommon shim

--- configs/product/fingerprint.mk
+++ configs/product/fingerprint.mk
@@ -17,5 +17,6 @@
 # Fingerprint
 PRODUCT_PACKAGES += \
     fingerprint.exynos5 \
+    libbauthtzcommon_shim \
     android.hardware.biometrics.fingerprint@2.1-impl \
     android.hardware.biometrics.fingerprint@2.1-service
